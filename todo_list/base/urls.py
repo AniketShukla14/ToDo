6f2e29f68dc243331ac1914770061e3919397a2d
AniketShukla14 <aniket14042001@gmail.com>
2022-05-25 15:22:42 +0530

    final touch

--- todo_list/base/urls.py
+++ todo_list/base/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import TaskList,TaskDetail,TaskCreate,TaskUpdate,DeleteView,CustomLoginView,LogoutView
+from .views import TaskList,TaskDetail,TaskCreate,TaskUpdate,DeleteView,CustomLoginView,LogoutView,registerPage, registerPage
 
 urlpatterns = [
     path('login/',CustomLoginView.as_view(), name='login'),
+    path('register/',registerPage.as_view(),name='register'),
     path('logout/',LogoutView.as_view(next_page='login'), name='logout'),
     path('',TaskList.as_view(),name='tasks'),
     path('task/<int:pk>/',TaskDetail.as_view(),name='task'),
